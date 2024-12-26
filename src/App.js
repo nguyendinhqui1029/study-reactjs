47a17f7143b341c721864ac3e68f1965a18368a3
nguyendinhqui1029 <nguyendinhqui1029@gmail.com>
2024-12-26 22:32:42 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -16,18 +16,18 @@ import {
   Switch,
   Route
 } from "react-router-dom";
-import Admin from "./screens/Admin/Admin";
-import Home from "./screens/Home/Home";
-import About from "./screens/About/About";
-import Product from './screens/Product/Product';
-import DetailProduct from './screens/DetailProduct/DetailProduct';
-import Contact from './screens/Contact/Contact';
-import News from './screens/News/News';
-import ExampleSource from './screens/ExampleSource/ExampleSource';
-import CartDetail from './screens/CartDetail/CartDetail';
-import ResultSearch from './screens/ResultSearch/ResultSearch';
-import MyOrder from './screens/MyOrder/MyOrder';
-import Login from './screens/Login/Login';
+import Admin from "src/screens/Admin/Admin";
+import Home from "src/screens/Home/Home";
+import About from "src/screens/About/About";
+import Product from 'src/screens/Product/Product';
+import DetailProduct from 'src/screens/DetailProduct/DetailProduct';
+import Contact from 'src/screens/Contact/Contact';
+import News from 'src/screens/News/News';
+import ExampleSource from 'src/screens/ExampleSource/ExampleSource';
+import CartDetail from 'src/screens/CartDetail/CartDetail';
+import ResultSearch from 'src/screens/ResultSearch/ResultSearch';
+import MyOrder from 'src/screens/MyOrder/MyOrder';
+import Login from 'src/screens/Login/Login';
 
 const iconList = Object.keys(Icons)
   .filter((key) => key !== "fas" && key !== "prefix")
